bcbd557585afc5c2bc0c086892fd584306b38660
VladaAlek <vladimiraleksic.nis@gmail.com>
2023-10-05 11:50:29 +0000

    ad event listener for document loading and button clicks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,22 @@
-function rungame() {
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listeners to them
+
+document.addEventListener("DOMContentLoaded", function () {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function () {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        });
+    }
+});
+
+function runGame() {
 
 }
 
@@ -10,26 +28,22 @@ function calculateCorrectAnswer() {
 
 }
 
-function incrementeScore() {
+function incrementScore() {
 
 }
 
-function incrementeWrongAnswer() {
-
+function incrementWrongAnswer() {
 
 }
 
 function displayAdditionQuestion() {
 
-
 }
 
 function displaySubtractQuestion() {
 
-
 }
 
 function displayMultiplyQuestion() {
-
 
 }

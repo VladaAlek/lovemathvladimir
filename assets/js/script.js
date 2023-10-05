78bc7a6a4d7446ccae91248b4754d69f37dab28f
VladaAlek <vladimiraleksic.nis@gmail.com>
2023-10-05 11:25:04 +0000

    feat: set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+function rungame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementeScore() {
+
+}
+
+function incrementeWrongAnswer() {
+
+
+}
+
+function displayAdditionQuestion() {
+
+
+}
+
+function displaySubtractQuestion() {
+
+
+}
+
+function displayMultiplyQuestion() {
+
+
+}
